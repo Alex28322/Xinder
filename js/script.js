f3b85b1f451a60ee92788dadb028751e67ca2c4e
Alex28322 <sander2832@gmail.com>
2025-03-03 16:01:31 +0200

    First

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const swiper = new Swiper('.swiper', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 0,       // No rotation
+        stretch: 0,      // No stretching
+        depth: 300,      // Depth effect
+        modifier: 1,     // Effect strength
+        slideShadows: false, // Disable shadows
+    },
+
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
